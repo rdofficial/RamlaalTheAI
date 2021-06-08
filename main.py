9edcbac20fb7761b9fafa69f5c0f4d97bfb2be87
Rishav Das <rdofficial192@gmail.com>
2021-06-08 15:45:46 +0530

    Updated the main.py script file. Added the main user commands input section for our AI. Currently just serving commands of playing music as well as exiting the program execution.

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ Last modified by : Rishav Das (https://github.com/rdofficial/)
 Last modified on : June 8, 2021
 
 Changes made in the last modification:
-1. Added the wishTheUser() function to the script, where our AI wishes the user as well as asks for their well being.
+1. Added the main user commands input section for our AI. Currently just serving commands of playing music as well as exiting the program execution.
 
 Authors contributed to this script (Add your name below if you have contributed) :
 1. Rishav Das (github:https://github.com/rdofficial/, email:rdofficial192@gmail.com)
@@ -25,6 +25,7 @@ Authors contributed to this script (Add your name below if you have contributed)
 try:
 	# Importing the pre-installed modules
 	from datetime import datetime
+	from webbrowser import open as webOpen
 
 	# Importing the externally installed libraries
 	import pyttsx3
@@ -51,15 +52,15 @@ def wishTheUser():
 	if time >= 0 and time < 12:
 		# If the timing is between 0 and 12 o clock, then we wish good morning
 
-		speak('Soo prabhat Saheb! I hope you have a good day Saheb.')
-	elif time >= 12 and hour < 18:
+		speak('Good morning! I hope you have a good day master.')
+	elif time >= 12 and time < 18:
 		# If the timing is between 12 and 18 o clock, then we wish good afternoon
 
-		speak('Subh din Saheb! I hope your day has went fine till now.')
+		speak('Good afternoon! I hope your day has went fine till now.')
 	else:
 		# If there are any other timings, then we wish good evening straight
 
-		speak('Andheri shyaam mei, diya tere haath mein! Saheb? I hope your day has went fine till now.')
+		speak('Good evening! I hope your day has went fine till now.')
 	del time
 
 	# Asking the user about them
@@ -81,6 +82,73 @@ def wishTheUser():
 
 def main():
 	wishTheUser()
+
+	# User command entry section
+	print('\n[ Enter the commands in your inputs to execute tasks ]')
+	while True:
+		# Asking the user for commands input in an infinite loop
+		text = input('Your input> ')
+
+		# Checking the user input
+		if 'play' in text.lower():
+			# If the term "play" is mentioned in the user input, then we continue to check further
+
+			if 'music' in text.lower() and len(text.split(' ')):
+				# If the term "music" is mentioned in the user input, then we continue to play music
+
+				speak('As per your request master, we are gonna play some music. Please choose any one of the options below.')
+				text = input('Choose :\n1. Play your favourite playlist\n2. Random songs from internet\nEnter your choice : ')
+				if text == '1':
+					# If the user entered the option for playing the favourite playlist, then we continue
+
+					speak('Extremely sorry master! This part of the AI is still under developement.')
+				elif text == '2':
+					# If the user entered option is for playing random songs through the internet, then we continue
+
+					speak('We are going to play songs from websites on internet, please choose any one of the options below.')
+					text = input('Choose :\n1. Play from youtube\n2. Play from spotify\n3. Play from gaana.com\nEnter your choice : ')
+
+					# Checking the user entered input for playing songs from the internet
+					if text == '1':
+						# If the user entered the option to play songs from youtube, then we continue to do so
+
+						webOpen('https://www.youtube.com/results?search_query=music')
+					elif text == '2':
+						# If the user entered the option to play songs from spotify, then we continue to do so
+
+						speak('Master! Spotify web service often requires to log in using an user account in order to play music. We will just redirect you to the website, and then you can browse through your specific flavoured songs.')
+						webOpen('https://open.spotify.com/')
+					elif text == '3':
+						# If the user entered the option to play songs from gaana.com, then we continue to do so
+
+						speak('Master! Gaana.com web service often requires custom search in order to play music. We will just redirect you to the website, and then you can browse through your specific flavoured songs.')
+						webOpen('https://gaana.com/')
+				else:
+					# If the user entered option is not recognized, then we pass and exit the current iteration
+
+					speak('There is some problem. Perhaps, you entered an invalid option. Let\'s proceed to the next command.')
+					continue
+		elif 'exit' in text.lower():
+			# If the term "exit" is mentioned in the user input, then we exit the script
+
+			# Confirming the user for the exit
+			speak('Master you asked for exit! Should we really exit the execution?')
+			text = input('Press Y / N : ')
+			if text.lower() == 'y' or text.lower() == 'yes':
+				# If the user entered yes, then we exit the script
+
+				speak('Bye bye master! We will miss you. Be happy and be carefull. Now, fuck off, too much emotional shitshow over here by me.')
+				exit()
+			elif text.lower() == 'n' or text.lower() == 'no':
+				# If the user entered no, then we do not exit the script
+
+				speak('Glad you want me serve more. For a while I thought, I did a mistake and now you will fucking kill me. Blessed that it is not that so. Let\'s continue.')
+				continue
+			else:
+				# If the user entered any other input than 'Y' or 'N', then we continue
+
+				speak('The decision input of yours is quite unrecongizable. But as an experience and computer machine, I guess we should continue serving is what you wanted to say.')
+				continue
 
 if __name__ == '__main__':
 	try:
